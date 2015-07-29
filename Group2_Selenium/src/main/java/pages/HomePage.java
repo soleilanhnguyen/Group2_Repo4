f05ba20a3d18eda70004898c961cf94a8ac78551
anhdung237 <anhdung237@gmail.com>
2015-07-29 08:30:58 +0700

    dung pham them cac comment vao Home page cho dep code

--- Group2_Selenium/src/main/java/pages/HomePage.java
+++ Group2_Selenium/src/main/java/pages/HomePage.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends AbstractPage {
-	// The constructor HomePage
+	/**
+	 * @description the contructor of Home page
+	 * @param driver
+	 */
 	public HomePage(WebDriver driver) {
 
 		PageFactory.initElements(driver, this);
@@ -16,15 +19,19 @@ public class HomePage extends AbstractPage {
 	 * @author Dung Pham
 	 * @param driver
 	 * @return BannerClientsPage
+	 * @description goto Banner Client Page
 	 */
 	public BannerClientsPage gotoBannerClient(WebDriver driver) {
 
 		selectMenuItem(bannerclientPath, driver);
 		return new BannerClientsPage(driver);
 	}
-	
-	// Navigate to WebLink
-	// Author: Anh Nguyen
+
+	/**
+	 * @author Anh Nguyen
+	 * @param driver
+	 * @return
+	 */
 	public WeblinksPage gotoWebLink(WebDriver driver) {
 
 		selectMenuItem(weblinkPath, driver);
@@ -32,21 +39,29 @@ public class HomePage extends AbstractPage {
 		return new WeblinksPage(driver);
 	}
 
+	/**
+	 * 
+	 * @param driver
+	 * @return
+	 */
 	public ArticlePage gotoArticlePage(WebDriver driver) {
 
 		selectMenuItem(articlePath, driver);
 
 		return new ArticlePage(driver);
 	}
-	
-	
+
+	/**
+	 * 
+	 * @param driver
+	 * @return
+	 */
 	public BannersPage gotoBannerPage(WebDriver driver) {
 
 		selectMenuItem(bannerPath, driver);
 
 		return new BannersPage(driver);
 	}
-	
 
 	/**
 	 * 
@@ -54,40 +69,44 @@ public class HomePage extends AbstractPage {
 	 * @description: Go to Category Manager Page "/"
 	 */
 
-	public CategoryManagerPage gotoCategoryManagerPage(WebDriver driver){
-		
+	public CategoryManagerPage gotoCategoryManagerPage(WebDriver driver) {
+
 		selectMenuItem(categorymanagertPath, driver);
-		
+
 		return new CategoryManagerPage(driver);
 	}
-	
+
 	/**
 	 * 
 	 * @author Ha Nguyen
 	 * @description: Go to Contact Page "/"
 	 */
-	
-	public ContactsPage gotoContactPage(WebDriver driver){
-		
+
+	public ContactsPage gotoContactPage(WebDriver driver) {
+
 		selectMenuItem(contactPath, driver);
-		
+
 		return new ContactsPage(driver);
 	}
-	
+
+	/**
+	 * 
+	 * @param driver
+	 * @return
+	 */
 	public CategoryBanner gotoCategoryBannerPage(WebDriver driver) {
 
 		selectMenuItem(categorybannerPath, driver);
 
 		return new CategoryBanner(driver);
 	}
-	
-	
-	private String bannerclientPath = "Components/Banners/Clients"; 
+
+	private String bannerclientPath = "Components/Banners/Clients";
 	private String weblinkPath = "Components/Weblinks/Links";
 	private String articlePath = "Content/Article Manager";
 	private String bannerPath = "Components/Banners/Banners";
 	private String categorybannerPath = "Components/Banners/CategoriesBanner";
-	private String contactPath ="Components/Contact/Contact";
+	private String contactPath = "Components/Contact/Contact";
 	private String categorymanagertPath = "Content/Category Manager";
 
 	@FindBy(xpath = "//a[contains(text(),'Components')]")
@@ -98,13 +117,11 @@ public class HomePage extends AbstractPage {
 
 	@FindBy(xpath = "//a[contains(text(),'Weblinks')]")
 	private WebElement MNU_COMPONENTS_WEBLINKS_LINKS;
-	
-	@FindBy(xpath="//a[contains(text(),'Contact')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Contact')]")
 	private WebElement MNU_COMPONENTS_CONTACTS;
-	
-	@FindBy(xpath="//a[contains(text(),'Contact')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Contact')]")
 	private WebElement MNU_COMPONENTS_CONTACTS_CONTACTS;
 
-
-	
 }
